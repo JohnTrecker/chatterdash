c209f7945685d176a4cd8d86b2b7133c20f06a14
johntrecker <jttrecker@gmail.com>
2017-04-29 00:56:16 -0700

    styles Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,27 @@ import Nav from './components/Nav.jsx';
 class App extends Component {
   constructor(){
     super();
-    this.state = {user: 'John Trecker'}
+    this.state = {
+      user: 'John Trecker',
+      room: 'Your Room'
+    }
+    this.setRoom.bind(this);
   }
+
+  setRoom(room){
+    this.setState({room: room});
+  }
+
+
+
   render() {
     return (
       // <Login/>
-      <Nav user={this.state.user}/>
+      <Nav
+        user={this.state.user}
+        room={this.state.room}
+        setRoom={this.setRoom}
+        timeOnline={'12'} />
     );
   }
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
-// import './Nav.css';
+import React from 'react';
+import './Nav.css';
 
 const Nav = (props) => {
-  <aside>
-    <h1>{props.user}</h1>
-  </aside>
+  return (
+    <aside>
+      <div className="header">
+        <h1>{props.user}</h1>
+        <h3>{`Online for ${props.timeOnline} minutes`}</h3>
+      </div>
+      <div className="rooms">
+        {
+          // props.rooms.map((room) => {
+          //   room.name = props.room ? return :
+          //     : <input
+          //         type='button'
+          //         key={room.id}
+          //         value={room.name}
+          //         onClick={props.setRoom(room.id)} />
+          // })
+        }
+      </div>
+    </aside>
+  )
 }
 
 export default Nav;
