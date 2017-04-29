9b3422cce6c84a238a677c97d4d83c9780500a4d
johntrecker <jttrecker@gmail.com>
2017-04-28 23:59:22 -0700

    adds Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Login from './components/Login.jsx';
+import Nav from './components/Nav.jsx';
 
 class App extends Component {
-
+  constructor(){
+    super();
+    this.state = {user: 'John Trecker'}
+  }
   render() {
     return (
-      <Login/>
+      // <Login/>
+      <Nav user={this.state.user}/>
     );
   }
 }
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import './Nav.css';
+
+const Nav = (props) => {
+  <aside>
+    <h1>{props.user}</h1>
+  </aside>
+}
+
+export default Nav;
