e57763005c7a37aa21b3e4b83add5c78eae7e5da
johntrecker <jttrecker@gmail.com>
2017-05-01 02:32:18 -0400

    splits app into Login and Main components instead of utilizing routes

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+import ChatRoom from './ChatRoom';
+import './Main.css';
+
+export default class Main extends Component {
+  constructor(){
+    super();
+    this.state = {
+      room: {},
+      rooms: [],
+      time: 0
+    }
+    this.newTime = this.newTime.bind(this)
+  }
+
+  setRoom(e){
+    const { value, id } = e.target;
+    this.setState({ room: {name: value, id: id} });
+  }
+
+  newTime(){
+    let newTime = this.state.time + 1
+    this.setState({time: newTime})
+  }
+
+  componentWillMount() {
+    fetch( 'http://localhost:8081/api/rooms' )
+      .then( res => res.json())
+      .then( rooms => this.setState( {rooms: rooms, room: rooms[0]} ))
+  }
+
+  render(props) {
+
+    let { user } = this.props
+    let { room, rooms, time } = this.state;
+
+    return (
+      <div id='Main'>
+
+        <Nav // props.user, API: rooms (obj),
+          user={user}
+          rooms={rooms}
+          time={time}
+          setRoom={this.setRoom.bind(this)} />
+
+        <ChatRoom
+          user={user}
+          room={room} />
+
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.newTime, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+}
